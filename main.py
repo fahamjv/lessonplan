63c4d8423f73118c1842e653223182bf2a406881
f4h4m <faham.javadi1@gmail.com>
2019-08-01 20:58:31 +0430

    just for backup

--- main.py
+++ main.py
@@ -1,10 +1,55 @@
 from flask import *
-from pprint import pprint
+from pprint import *
 from datetimerange import DateTimeRange
-
 from datetime import *
+import pymysql.cursors
 
 app = Flask(__name__)
+
+
+
+def same_time(rows):
+    ll = []
+    counter = 0
+
+    for i in range(0, len(rows)):
+        for j in range(i + 1, len(rows)):
+            if rows[i]['time'].is_intersection(rows[j]['time']):
+                counter += 1
+                ll.append([rows[i]['id'],rows[j]['id']])
+
+    return ll
+
+def same_days(rows,day_name):
+    ll = []
+    counter = 0
+
+    for item in rows:
+        if item['day'] == day_name :
+            counter+=1
+            ll.append(item)
+
+    if counter > 1:
+        return ll
+    else:
+        return []
+
+def make_rows(rows):
+    ll = []
+
+    for item in rows:
+
+        collection = {
+            'id': item['id'],
+            'day': item['day'],
+            'time': DateTimeRange("2019-03-22T%s+0900" % item['start_time'], "2019-03-22T%s+0900" % item['end_time']),
+        }
+
+        ll.append(collection)
+
+    return ll
+
+
 
 @app.route('/')
 def main():
@@ -12,36 +57,145 @@ def main():
 
 
 
+
+@app.route('/check')
+def check():
+    try:
+        connection = pymysql.connect(host='localhost',
+                                     user='root',
+                                     password='',
+                                     db='classplanning',
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM `classes`"
+            cursor.execute(sql)
+            result = cursor.fetchall()
+
+            rows = make_rows(result)
+
+            return str(rows)
+
+
+            ll = []
+
+            for item in ['shanbe', 'yekshanbe', 'doshanbe', 'seshanbe', 'chaharshanbe']:
+                day = same_days(rows, item)
+                time = same_time(day)
+                if (time):
+                    ll.append(time[0])
+
+            return jsonify(ll)
+
+
+            for item in result:
+                item['start_time'] = item['start_time'][:-3]
+                item['end_time'] = item['end_time'][:-3]
+                item['remove'] = "<a href='%s'>Remove</a>" % url_for('remove', id=item['id'])
+
+            return jsonify(result)
+
+    finally:
+        connection.close()
+
+
+
+
+@app.route('/add',methods=["POST"])
+def add():
+
+    startTime = "%s:%s" % (request.form.get('start_time'),'01')
+    endTime = "%s:%s" % (request.form.get('end_time'),'01')
+    day = request.form.get('day')
+    lessonCode = request.form.get('lesson_code')
+
+
+
+    try:
+        connection = pymysql.connect(host='localhost',
+                                     user='root',
+                                     password='',
+                                     db='classplanning',
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+        with connection.cursor() as cursor:
+            # Create a new record
+            sql = "INSERT INTO `classes` (`id`, `lesson_code`, `day`, `start_time`, `end_time`) VALUES (NULL, %s, %s , %s, %s)"
+            cursor.execute(sql, (lessonCode, day, startTime, endTime))
+
+        connection.commit()
+
+    finally:
+        connection.close()
+        return redirect('/')
+
+
+
+    return  startTime + '<br>' + endTime + '<br>' + day + '<br>' + lessonCode
+
+
+
+@app.route('/remove/<id>')
+def remove(id):
+
+    try:
+        connection = pymysql.connect(host='localhost',
+                                     user='root',
+                                     password='',
+                                     db='classplanning',
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
+        with connection.cursor() as cursor:
+            sql = "DELETE FROM `classes` WHERE `classes`.`id` = %s"
+            cursor.execute(sql, (id))
+
+
+        connection.commit()
+
+    finally:
+        connection.close()
+        return redirect('/')
+
+
+
 @app.route('/columns')
 def columns():
     d = [
           {"name":"id","title":"ID","breakpoints":"xs sm","type":"number","style":{"width":80,"maxWidth":80}},
-          {"name":"day","title":"Day Name"},
-          {"name":"datetime","title":"Time"},
+          {"name": "day", "title": "Day Name"},
+          {"name": "lesson_code","title":"Lesson code"},
+          {"name":"start_time","title":"Start Time"},
+          {"name":"end_time","title":"End Time"},
+          {"name":"remove","title":"Remove"},
         ]
     return jsonify(d)
 
 @app.route('/rows')
 def rows():
-    d = [
-        {
-            'id': 1,
-            'day': 'shanbe',
-            'datetime': DateTimeRange("2019-03-22T8:00:00+0900", "2019-03-22T10:00:00+0900"),
-            # 'datetime' :  datetime.now(),
-        },
-    ]
+    try:
+        connection = pymysql.connect(host='localhost',
+                                     user='root',
+                                     password='',
+                                     db='classplanning',
+                                     charset='utf8mb4',
+                                     cursorclass=pymysql.cursors.DictCursor)
 
-    print(d)
+        with connection.cursor() as cursor:
+            sql = "SELECT * FROM `classes`"
+            cursor.execute(sql)
+            result = cursor.fetchall()
 
-    return 'asd'
-    d = [
-        {
-            'id':1,
-            'day':'shanbe',
-            'datetime' :  DateTimeRange("2019-03-22T8:00:00+0900", "2019-03-22T10:00:00+0900"),
-            # 'datetime' :  datetime.now(),
-        },
-    ]
-    return jsonify(d)
+            for item in result:
+                item['start_time'] = item['start_time'][:-3]
+                item['end_time'] = item['end_time'][:-3]
+                item['remove'] = "<a href='%s'>Remove</a>" % url_for('remove',id=item['id'])
+
+            return jsonify(result)
+
+    finally:
+        connection.close()
+
+
+
 
--- new.py
+++ new.py
@@ -7,18 +7,18 @@ def same_time(rows):
 
     for i in range(0, len(rows)):
         for j in range(i + 1, len(rows)):
-            if rows[i]['datetime'].is_intersection(rows[j]['datetime']):
+            if rows[i]['time'].is_intersection(rows[j]['time']):
                 counter += 1
                 ll.append([rows[i]['id'],rows[j]['id']])
 
     return ll
 
 
-def same_days(data,day_name):
+def same_days(rows,day_name):
     ll = []
     counter = 0
 
-    for item in data:
+    for item in rows:
         if item['day'] == day_name :
             counter+=1
             ll.append(item)
@@ -29,39 +29,66 @@ def same_days(data,day_name):
         return []
 
 
+def make_rows(rows):
+    ll = []
+    for item in rows:
+        collection = {
+            'id': item['id'],
+            'day': item['day'],
+            'time': DateTimeRange("2019-03-22T%s+0900" % item['time']['start'], "2019-03-22T%s+0900" % item['time']['end']),
+        }
+
+        ll.append(collection)
+
+    return ll
 
 
-rows = [
+before = [
     {
-        'id':1,
-        'day':'shanbe',
-        'datetime' :  DateTimeRange("2019-03-22T8:00:00+0900", "2019-03-22T10:00:00+0900"),
+        'id': 1,
+        'day': 'shanbe',
+        'time': {
+            'start':'8:00:00',
+            'end':'10:00:00',
+        }
     },
     {
         'id': 2,
-        'day':'doshanbe',
-        'datetime' :  DateTimeRange("2019-03-22T9:30:00+0900", "2019-03-22T11:00:00+0900"),
+        'day': 'doshanbe',
+        'time': {
+            'start': '9:30:00',
+            'end': '11:00:00',
+        }
     },
     {
         'id': 3,
-        'day':'yekshanbe',
-        'datetime': DateTimeRange("2019-03-22T10:01:00+0900", "2019-03-22T14:30:00+0900"),
+        'day': 'shanbe',
+        'time': {
+            'start': '9:00:00',
+            'end': '14:30:00',
+        }
     },
     {
         'id': 4,
-        'day':'seshanbe',
-        'datetime': DateTimeRange("2019-03-22T9:00:00+0900", "2019-03-22T12:00:00+0900"),
+        'day': 'shanbe',
+        'time': {
+            'start': '9:30:00',
+            'end': '12:00:00',
+        }
     }
 ]
 
+rows = make_rows(before)
+
+
 ll = []
 
-for item in ['shanbe','yekshanbe','doshanbe','seshanbe','chaharshanbe']:
+for item in ['saturday','sunday','monday','tuesday','wednesday']:
     day = same_days(rows,item)
     time = same_time(day)
     if(time):
-        ll.append(time[0])
+        ll.append(time)
 
 
-print(ll)
+print(ll[0])
 
